d92614c2e39f5389998ec3cb8a60638e4e513b3d
Paolo <sba24150@student.cct.ie>
2024-12-18 18:19:15 +0000

    updated task 4
    wrote a much better  way to try and catch the exception

--- task4/src/task4/Task4.java
+++ task4/src/task4/Task4.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package task4;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  *
@@ -15,37 +16,72 @@ public class Task4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //https://github.com/sba24150/CA2-Programming-and-Math-Paolo-Mazzocco
+        
         System.out.println("hello");
         // sc is object of scanner class
         Scanner sc = new Scanner(System.in); // Its create object of scanner class
             try{
-            // Input size of the square matrix
-            System.out.print("Enter size of the row matrix: ");
-            int row = sc.nextInt();// we use sc to access function of scanner class.Here function is nextint
-
-            System.out.print("Enter size of the coloum matrix: ");
-            int col = sc.nextInt();
-            if (col<=0 || row<=0){
-                    System.out.println("Entered number not valid must be greater than 0 ");
-                            }else{
+               int row = 0;
+               int col = 0;
+                while(true){
+                    try{
+                      // Input size of the square matrix
+                    System.out.print("Enter size of the row matrix: ");
+                    row = sc.nextInt();// we use sc to access function of scanner class.Here function is nextint
+                    if (row<=0){
+                       System.out.println("Entered number not valid must be greater than 0 "); 
+                        }else{
+                            break;
+                        }
+                        }catch(InputMismatchException e){
+                            System.out.println("Invalid data type, enter a valid number");
+                            sc.nextLine();
+                        }
+                    }    
+                while(true){
+                    try{
+                    // Input size of the square matrix
+                    System.out.print("Enter size of the colomn matrix: ");
+                    col = sc.nextInt();// we use sc to access function of scanner class.Here function is nextint
+                    if (col<=0){
+                        System.out.println("Entered number not valid must be greater than 0 "); 
+                        }else{
+                            break;
+                        }
+                    }
+                    catch(InputMismatchException e){
+                             System.out.println("Invalid data type, enter a valid number");
+                             sc.nextLine();
+                        }
+                    }
+                    
                 // Initialize the square matrix
                 int[][] matrix = new int[row][col];
                 
                 // Input elements row by row
                 System.out.println("Enter elements :");
 
-                        for (int i = 0; i < row; i++) {       
-                    for (int j = 0; j< col; j++) {
-                        matrix[i][j] = sc.nextInt();
-                               }
+                    for (int i = 0; i < row; i++) {       
+                        for (int j = 0; j< col; j++) {
+                            while(true){
+                                try{
+                                    matrix[i][j] = sc.nextInt();
+                                    break;
+                                }catch(InputMismatchException e){
+                             System.out.println("Invalid data type, enter a valid number");
+                             sc.nextLine();
+                            }
+                        }    
+                    }
                 }
                 // To display the matrix in 2D Form
 
                 for(int i=0; i<row; i++){
                         for(int j=0; j<col; j++){
-                                System.out.print(matrix[i][j]+"  ");
+                            System.out.print(matrix[i][j]+"  ");
                         }
-                                System.out.println("");
+                            System.out.println("");
                 }
                 System.out.print("Spiral traversal of the matrix : " );
                 // definig the 4 starting point from where the loop will start at time 
@@ -70,22 +106,26 @@ public class Task4 {
                     right--;
                    
                     //right to left
-                    for (int i =right; i>=left; i--){
-                        System.out.print(matrix[bottom][i]);
-                        System.out.print(" ");
-                    }
-                    bottom--;
+                    if (top <= bottom) {
+                        for (int i =right; i>=left; i--){
+                            System.out.print(matrix[bottom][i]);
+                            System.out.print(" ");
+                        }
+                        bottom--;
+                    }    
                     //bottom to top
-                    for(int i =bottom; i>=top; i--){
-                        System.out.print(matrix[i][left]);
-                        System.out.print(" ");
+                    if (left <= right) {
+                        for(int i =bottom; i>=top; i--){
+                            System.out.print(matrix[i][left]);
+                            System.out.print(" ");
+                        }
+                        left++;
                     }
-                    left++;
                 }
-            }
-        }catch (Exception e) {
-        System.out.println("User input is not a number.");
-    }          
+            }catch (Exception e) {
+        System.out.println("An error occurred: " + e.getMessage() );
+    } 
+                 
     }
     
 }
